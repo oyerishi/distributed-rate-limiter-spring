ebe56bbe7a81e840917a1e47e7bf546201e9b4dc
oyerishi <oyerishiii@gmail.com>
2026-03-30 04:25:38 +0530

    Integration: Linked RateLimitInterceptor to RedisRateLimiterService

--- src/main/java/com/example/ratelimiter/interceptor/RateLimiterInterceptor.java
+++ src/main/java/com/example/ratelimiter/interceptor/RateLimiterInterceptor.java
@@ -2,6 +2,7 @@ package com.example.ratelimiter.interceptor;
 
 import com.example.ratelimiter.annotation.RateLimited;
 import com.example.ratelimiter.service.RateLimiterService;
+import com.example.ratelimiter.service.RedisRateLimiterService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +13,8 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 @RequiredArgsConstructor
 public class RateLimiterInterceptor implements HandlerInterceptor {
-    private final RateLimiterService service;
-
+//    private final RateLimiterService service;     //PostgreSQL
+    private final RedisRateLimiterService service; //Redis
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
         //is this request hitting a real java method(Controller)?
@@ -24,7 +25,7 @@ public class RateLimiterInterceptor implements HandlerInterceptor {
             if(method.hasMethodAnnotation(RateLimited.class)){
                 String clientId = request.getRemoteAddr();
 
-                if(!service.allowRequest(clientId)){
+                if(!service.isAllowed(clientId, 2, 1)){
                     response.setStatus(429);
                     response.getWriter().write("Rate Limit Exceeded via Interceptor!");
                     return false;
