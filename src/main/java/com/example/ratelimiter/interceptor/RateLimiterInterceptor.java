2aefd6396e181cd7ecc4f64af817e414b112078e
oyerishi <oyerishiii@gmail.com>
2026-03-30 04:52:43 +0530

    V2 Complete: Fully distributed rate Limiter with Redis, Lua, and dynamic properties

--- src/main/java/com/example/ratelimiter/interceptor/RateLimiterInterceptor.java
+++ src/main/java/com/example/ratelimiter/interceptor/RateLimiterInterceptor.java
@@ -6,6 +6,7 @@ import com.example.ratelimiter.service.RedisRateLimiterService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -15,6 +16,12 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class RateLimiterInterceptor implements HandlerInterceptor {
 //    private final RateLimiterService service;     //PostgreSQL
     private final RedisRateLimiterService service; //Redis
+
+    @Value("${app.rate-limit.capacity}")
+    private int capacity;
+    @Value("${app.rate-limit.refill-rate}")
+    private int refillRate;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
         //is this request hitting a real java method(Controller)?
@@ -25,7 +32,7 @@ public class RateLimiterInterceptor implements HandlerInterceptor {
             if(method.hasMethodAnnotation(RateLimited.class)){
                 String clientId = request.getRemoteAddr();
 
-                if(!service.isAllowed(clientId, 2, 1)){
+                if(!service.isAllowed(clientId, capacity, refillRate)){
                     response.setStatus(429);
                     response.getWriter().write("Rate Limit Exceeded via Interceptor!");
                     return false;
